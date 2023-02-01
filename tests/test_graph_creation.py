2665cd17d154b5f219fc1db7911508aaf1fbfe3e
melkisedeath <manoskaristineos@gmail.com>
2023-02-01 15:37:53 +0100

    test for graph creation.

--- tests/test_graph_creation.py
+++ tests/test_graph_creation.py
@@ -3,6 +3,7 @@ import os
 import partitura as pt
 from graphmuse.utils.graph import edges_from_note_array
 import numpy as np
+from graphmuse.samplers import compute_edge_list
 
 
 class TestGraphMuse(unittest.TestCase):
@@ -14,5 +15,5 @@ class TestGraphMuse(unittest.TestCase):
         score = pt.load_score(score_path)
         note_array = score.note_array()
         edges_python = np.sort(edges_from_note_array(note_array))
-        edges_c = np.sort([])
+        edges_c = np.sort(compute_edge_list(note_array["onset_div"], note_array["duration_div"]))
         self.assertEqual(edges_python, edges_c)
