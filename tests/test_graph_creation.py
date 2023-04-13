e61bf02ed9f9ed6b7edab67fa48c3bf116018bfa
nimrodVarga <nimrodvarga@hotmail.com>
2023-04-13 12:57:45 +0200

    shape of numpy arrays are asserted to be equal before arrays are asserted to be equal

--- tests/test_graph_creation.py
+++ tests/test_graph_creation.py
@@ -49,21 +49,24 @@ def edges_from_note_array(note_array):
     edges = np.array([edg_src, edg_dst])
     return edges
 
+
 class TestGraphMuse(unittest.TestCase):
     """
     Tests for graphmuse graph creation.
     """
     def test_edge_list(self):
+        
+
         score_path = os.path.join(os.path.dirname(__file__), "samples", "wtc1f01.musicxml")
         score = pt.load_score(score_path)
         note_array = score.note_array()
         edges_python = np.sort(edges_from_note_array(note_array))
 
-
         edge_list, edge_types = sam.compute_edge_list(note_array['onset_div'].astype(np.int32), note_array['duration_div'].astype(np.int32))
 
         edges_c = np.sort(edge_list)
 
-        self.assertTrue((edges_c==edges_python).all())
+        self.assertTrue(edges_c.shape==edges_python.shape)
 
-        print('edge list test passed')
+        self.assertTrue((edges_c==edges_python).all())
+        
