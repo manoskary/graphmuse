5cd0b79ad90ae99e1f4e2ab863e7fb14f6a9f495
melkisedeath <manoskaristineos@gmail.com>
2024-01-05 15:00:05 +0100

    minor corrections.

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -44,27 +44,28 @@ class TestSamplers(unittest.TestCase):
             for l in range(depth):
                 self.assertTrue(torch.all(torch.unique(torch.hstack((samples_per_layer[l], samples_per_layer[l+1]))) == load_per_layer[l].sort()[0]))
                 current_edges = edges_between_layers[l]
-                current_edges_list = [(current_edges[0, i], current_edges[1, i]) for i in range(current_edges.shape[1])]
                 # Check that each node is sampled at most once
                 samples_counter = dict()
-                for s,d in current_edges_list:
-                    if d in samples_counter.keys():
-                        samples_counter[d] += 1
+                for edge in current_edges.t():
+                    src = edge[0].item()
+                    dst = edge[1].item()
+                    if dst in samples_counter.keys():
+                        samples_counter[dst] += 1
                     else:
-                        samples_counter[d] = 1
+                        samples_counter[dst] = 1
 
-                for d, c in samples_counter.items():
-                    self.assertTrue(c == min(samples_per_node, g.preneighborhood_count(d)), f"count of {d} is {c}, but pnc is {g.preneighborhood_count(d)}")
+                for dst, c in samples_counter.items():
+                    self.assertTrue(c == min(samples_per_node, g.preneighborhood_count(dst)), f"count of {dst} is {c}, but pnc is {g.preneighborhood_count(dst)}")
 
                 unique_src = np.unique(current_edges[0])
 
                 self.assertTrue(unique_src.shape == samples_per_layer[l].shape)
                 self.assertTrue(list(unique_src) == sorted(samples_per_layer[l]))
 
-                unique_dst = set(current_edges[1])
+                unique_dst = current_edges[1]
 
                 for sample in samples_per_layer[l+1]:
                     if sample not in unique_dst:
-                        self.assertTrue(g.preneighborhood_count(sample) == 0)
+                        self.assertTrue(g.preneighborhood_count(sample.item()) == 0)
 
             print(f"{nodewise_sampling_method.__name__} passed all tests")
